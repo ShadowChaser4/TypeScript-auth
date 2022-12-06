3fb767e217c13cb7542d5add5c58d79c0dbb0e71
ShadowChaser4 <kushalacharya48v@outlook.com>
2022-12-06 16:23:50 +0545

    Created bycrypt based encryption

--- src/auth/PassportLocal.ts
+++ src/auth/PassportLocal.ts
@@ -2,6 +2,7 @@ import passport from "passport";
 import passportLocal from 'passport-local'
 import { IUser, User } from "../db/UserStrategy";
 import { CallbackError, HydratedDocument } from "mongoose";
+import {compare} from 'bcrypt';
 const LocalStrategy = passportLocal.Strategy
 
 
@@ -9,33 +10,30 @@ const LocalStrategy = passportLocal.Strategy
 
 passport.use(
   new LocalStrategy( (
-    function verify(username:string, password_entered:string, cb:Function){
-      
-     User.findOne({email:username}, 
-      function(err:CallbackError, user:IUser )
-      {
-        
-        if (err)
-        {
-          return cb(err, false)
-        }
+ async function verify(username:string, password_entered:string, cb:Function){
+    try{  
+    const user: IUser|null = await User.findOne({email:username.toLowerCase()})
 
-        if (! user)
-        {
-          return cb(null , false)
-        }
+    if (! user) return cb(null, false)
 
-        const {email,first_name, middle_name, last_name , roles} = user
+    const match: Boolean = await compare(password_entered, user.password)
+    
+    if (! match) return cb(null, false)
 
-        return cb(null , {email, first_name, middle_name, last_name, roles})
+    const {email,first_name, middle_name, last_name , roles} = user
 
-      }
-      ) 
+    return cb(null , {email, first_name, middle_name, last_name, roles})
+
        
-
-        
-
     }
+    catch(err)
+    {
+      if (err instanceof Error)
+      {
+        cb(err, false)
+      }
+    }
+  }
   )) 
 )
 
--- src/routes/authroutes/register.ts
+++ src/routes/authroutes/register.ts
@@ -1,7 +1,7 @@
 import { IUser, User} from "../../db/UserStrategy";
 import { Router, Request,Response } from "express";
 import { CallbackError, HydratedDocument } from "mongoose";
-
+import { hash } from "bcrypt";
 
 const router:Router = Router(); 
 
@@ -9,23 +9,34 @@ const router:Router = Router();
 
 
 router.post("", 
-   (req:Request<IUser>, res:Response):void=>
+  async (req:Request<IUser>, res:Response):Promise<void>=>
     {
+      try{
       //  if(! req.isAuthenticated() ) 
       //  {
       //   res.status(403)
       //   res.send({"message":"not authorized"})
       //   return
       //  }
-            const {email, first_name, middle_name, last_name,password, role} = req.body
+            const {email, first_name, middle_name, last_name,password, roles} = req.body
+            
+            const emailpattern:RegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
+
+            if (!emailpattern.test(email)) 
+            {
+                  res.status(400)
+                  res.json({"message":"Bad request, email invalid"})
+                  return 
+            }
+            const hashed_password:string = await hash(password, 10)
 
             const user: HydratedDocument<IUser> = new User({
                 first_name: first_name, 
                 middle_name: middle_name, 
                 last_name : last_name, 
-                email:email, 
-                password:password, 
-                role:role
+                email:email.toLowerCase(), 
+                password: hashed_password, 
+                roles:roles
                 
             })
             
@@ -40,13 +51,22 @@ router.post("",
                   
                    res.json({
                     user
-                })
+                         })
 
                }
             )
            
-
-    })
+            }
+            catch(error)
+            {
+               if (error instanceof Error)
+               {
+                  res.status(500)
+                  res.json({"error":error.name})
+               }
+            }
+    }
+    )
 
 export{router}
 
